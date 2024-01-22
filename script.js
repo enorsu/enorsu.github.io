e7df8c4616579a4bb24892e5578e8197381334df
SotaNorsu <105721278+enorsu@users.noreply.github.com>
2024-01-22 16:57:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+
+
+// made by neel shah (not me)
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}​
+
+var id = GetURLParameter("id")
+
+if (id == "inst") {
+  this.window = "https://github.com/enorsu"
+}
