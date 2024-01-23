b0e03010f1f6e073b384d78d54ee1b18b373e59a
SotaNorsu <pelaaja538@gmail.com>
2024-01-23 17:09:48 +0200

    Update

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 
-
-
-
+//github redirect
 function redirect_github() {
-  window.location = "https://github.com/enorsu"
+  window.location = "https://github.com/enorsu";
 }
+
+// redirect function for easy access
+function goto(URL) {
+  window.location = URL;
+}
+
+
+//redirects
+const queryString =  window.location.search;
+console.log(queryString);
+
+const urlParams = new URLSearchParams(queryString);
+
+const redir = urlParams.get("r");
+
+if (redir = "github") {goto("https://github.com/enorsu");}
+
