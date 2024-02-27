dee633e9be22a43bc302dcc51e9ef55d6b485c9a
SotaNorsu <pelaaja538@gmail.com>
2024-02-27 16:26:20 +0200

    Edited index.html
    and css modifications

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ function redirect_github() {
   window.location.href = "https://github.com/enorsu";
 }
 
+//simple note app redirect function
+function redirect_notes() {
+  window.location.href = "simple-note-web-app";
+}
+
 function doit() {
   alert("wait, that's illegal");
 }
