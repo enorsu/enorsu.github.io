156871965c9a41e6dfe9629b87f7c6a6446ea11c
SotaNorsu <pelaaja538@gmail.com>
2024-02-27 11:09:16 +0200

    Updated website
    Added proper css

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 
-//github redirect
+//github redirect function
 function redirect_github() {
-  window.location = "https://github.com/enorsu";
+  window.location.href = "https://github.com/enorsu";
 }
-
-
-//redirects
-const queryString =  window.location.search;
-console.log(queryString);
-
-const urlParams = new URLSearchParams(queryString);
-
-const redir = urlParams.get("r");
-
-if (redir = "github") {redirect_github();}
 
